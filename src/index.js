60fe106a6bebcdf1bee857a6a39f540477a80aab
EvgeniyGorbunov <yhgamuro@gmail.com>
2019-10-03 15:11:56 +0300

    feat: 13 test passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let res = str;
+    if(str%2){
+        return false;
+    }
+    for(let i=0;i<res.length;i++){
+        for(let j=0;j<bracketsConfig.length;j++){
+            if(bracketsConfig[j][0]==res[i]){
+                stack.push(res[i]);
+            }else if(bracketsConfig[j][1]==res[i]){
+                if(stack[stack.length-1]==bracketsConfig[j][0]){
+                    stack.splice(stack.length-1, 1);
+                 
+                }else return false;
+            }
+        }
+    }
+    console.log(stack);
+    return stack.length>0 ? false : true;
 }
+
